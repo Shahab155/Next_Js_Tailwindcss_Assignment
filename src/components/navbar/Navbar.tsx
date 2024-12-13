6613c5a28265e91d0d25f7b4e16ff4150236b257
shahab2015 <166250566+shahab2015@users.noreply.github.com>
2024-12-13 03:18:56 -1100

    first commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,9 +7,9 @@ export default function Navbar(props:navbarProp) {
    <>
    <nav className='mx-16 my-16 '>
     <ul className= {`${props.font.className} flex items-center justify-end space-x-5 font-medium text-xl `}>
-        <li className='hover:underline'><Link href='/'>Works</Link></li>
-        <li className='hover:underline'><Link href='/'>Blog</Link></li>
-        <li className='hover:underline'><Link href='/'>Contact</Link></li>
+        <li className='hover:underline'><Link href='#!'>Works</Link></li>
+        <li className='hover:underline'><Link href='#!'>Blog</Link></li>
+        <li className='hover:underline'><Link href='#!'>Contact</Link></li>
     </ul>
    </nav>
    </>
